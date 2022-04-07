7557e72f354f1e92222b8ecef88c7f8ca022158c
EssMarimbi <marimbi.esther@gmail.com>
2022-04-07 15:19:02 -0700

    My forth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
+
+0;276;0c#include <stdio.h>
 
 /**
- *main - Entry point
- *
- *Return: ALways 0 (Success)
- */
+*main - Entry point
+*
+*Return: ALways 0 (Success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
- return (0);
+return (0);
 }
