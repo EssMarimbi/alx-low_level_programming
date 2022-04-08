eff137e1f2cf56bf61551230db60800b31b26b63
EssMarimbi <marimbi.esther@gmail.com>
2022-04-07 18:14:07 -0700

    My task 6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long int b;
 long long int c;
 char d;
 float f;
-printf("size of a char: 1 byte(s)\n", sizeof(d));
-printf("size of an int: 4 byte(s)\n", sizeof(a));
-printf("size of a long int: 4 byte(s)\n", sizeof(b));
-printf("size of a long long int: 8 byte(s)\n", sizeof(c));
-printf("size of a float: 4 byte(s)\n", sizeof(f));
+printf("size of a char: 1 byte(s)\n" ;
+printf("size of an int: 4 byte(s)\n" ;
+printf("size of a long int: 4 byte(s)\n" ;
+printf("size of a long long int: 8 byte(s)\n" ;
+printf("size of a float: 4 byte(s)\n";
 return (0);
 }
