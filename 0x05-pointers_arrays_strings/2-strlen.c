b2a9890844ddfcf32a2f996d864899d6354cbb97
EssMarimbi <marimbi.esther@gmail.com>
2022-04-20 16:45:07 -0700

    My string length commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,16 @@
 #include "main.h"
-
 /**
-* * _strlen - measures the length of a string
-* * @s: input
-* * Return: 0 if successful
+*  _strlen - measures the length of a string
+*  @s: input
+*  Return: 0 if successful
 */
 
-int _strlen(char *s);
+int _strlen(char *s)
 {
-int length = 0;
-while (*s ! =0)
+int count = 0;
+while (*s[count] ! '\0')
 {
-length++;
-s++;
+count++;
 }
-return (length);
+return (count);
 }
