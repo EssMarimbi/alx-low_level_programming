c693f5c3f96cf9cc124efa58e843d9949c2717d5
EssMarimbi <marimbi.esther@gmail.com>
2022-04-20 16:06:20 -0700

    Length of a string commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+* strlen - measures the length of a string
+* @s: input
+* Return: 0 if successful
+*/
+
+int _strlen(char *s)
+{
+int length = 0;
+while (*s ! =0)
+{
+length++;
+s++;
+}
+return (length);
+}
