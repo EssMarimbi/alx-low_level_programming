553c122844f085375954c612d3481a71eb040dda
EssMarimbi <marimbi.esther@gmail.com>
2022-04-20 15:38:27 -0700

    Test commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* reset_to_98 - points to an integer and updates its value
+* @n: pointer at the variable n
+* Return: 0 if successful
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}  
